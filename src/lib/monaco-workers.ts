8662eb9af954959f9eacec5e3c4f37345f07c860
alesandr2021@gmail.com App-Lab-Hub <alesandr2021@gmail.com>
2026-06-02 22:23:31 +0500

    Work strange

--- src/lib/monaco-workers.ts
+++ src/lib/monaco-workers.ts
@@ -3,6 +3,7 @@ import JsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
 import CssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
 import HtmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import TsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+import "monaco-editor/min/vs/editor/editor.main.css"; // Здесь лежит разметка виджета
 
 export function initMonacoWorkers() {
   if (typeof window === "undefined") return; // Защита от SSR (сервера)
