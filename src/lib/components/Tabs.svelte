516019585efa6dbd25984d1d8d866109c7c460d5
alesandr2021@gmail.com App-Lab-Hub <alesandr2021@gmail.com>
2026-07-16 00:01:01 +0500

    fixs tab anim

--- src/lib/components/Tabs.svelte
+++ src/lib/components/Tabs.svelte
@@ -25,7 +25,7 @@
 <div class="flex items-center w-full sm:w-auto">
   <div 
     data-active={filterType}
-    class="tabs-container relative flex items-center w-full sm:w-fit dark:bg-card/30 light:bg-purple-200/30 p-1 rounded-xl border dark:border-border/50 light:border-purple-300/40 h-10 min-w-[200px]"
+    class="justify-around tabs-container relative flex items-center w-full sm:w-fit dark:bg-card/30 light:bg-purple-200/30 p-1 rounded-xl border dark:border-border/50 light:border-purple-300/40 h-10 min-w-[200px]"
   >
     <!-- Плавающая плашка -->
     <div class="tab-indicator absolute top-1 bottom-1 left-1 rounded-lg z-10 shadow-lg shadow-primary/20"
